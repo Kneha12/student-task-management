2b4cd81cbe10d8ceba0a5006fd6ff13af205a8f2
Neha Kumari <117574008+Kneha12@users.noreply.github.com>
2026-03-11 16:10:30 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'student-task-manager',
+  webDir: 'www'
+};
+
+export default config;
