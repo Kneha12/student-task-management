33187a55d5b221c68995bb6b129305c70bfe335a
Neha Kumari <117574008+Kneha12@users.noreply.github.com>
2026-03-11 16:50:49 +0530

    make structure and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,20 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },  {
+    path: 'add-task',
+    loadComponent: () => import('./pages/add-task/add-task.page').then( m => m.AddTaskPage)
   },
+  {
+    path: 'task-details',
+    loadComponent: () => import('./pages/task-details/task-details.page').then( m => m.TaskDetailsPage)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
+  },
+
 ];
