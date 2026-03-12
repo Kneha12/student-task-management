480787086e8986312d41e29d2e5020bd4d2338cb
Neha Kumari <117574008+Kneha12@users.noreply.github.com>
2026-03-12 18:06:30 +0530

    added dark and light mode and some changes

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,18 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenu, IonButton, IonMenuButton, IonButtons, IonAvatar, IonLabel, IonList, IonItem, IonIcon, IonListHeader, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonGrid, IonRow, IonCol, IonProgressBar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonMenu,
+  IonButton,
+  IonMenuButton,
+  IonButtons,
+  IonAvatar,
+  IonLabel,
+  IonList,
+  IonItem,
+  IonIcon,
+  IonListHeader,
+  IonCard,
+  IonProgressBar, IonCardHeader, IonCardTitle, IonCardContent } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { TaskCardComponent } from '../../components/task-card/task-card.component';
-import { 
-  gridOutline, 
-  bookOutline, 
-  calendarNumberOutline, 
-  alarmOutline, 
-  statsChartOutline, 
-  timerOutline, 
+import {
+  gridOutline,
+  bookOutline,
+  calendarNumberOutline,
+  alarmOutline,
+  statsChartOutline,
+  timerOutline,
   settingsOutline,
-  // checkmarkDoneCircleOutline 
+  sunnyOutline,
+  moonOutline,
+  addOutline,
+  hourglassOutline
+  // checkmarkDoneCircleOutline
 } from 'ionicons/icons';
 
 @Component({
@@ -20,46 +40,81 @@ import {
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonProgressBar, IonCard, IonListHeader, IonIcon, IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenu, IonMenuButton, IonAvatar, IonLabel, IonItem, TaskCardComponent, IonButton, IonGrid, IonRow, IonCol, IonCardHeader, IonCardTitle, IonCardContent]
+  imports: [IonCardContent, IonCardTitle, IonCardHeader, 
+    IonProgressBar,
+    IonCard,
+    IonListHeader,
+    IonIcon,
+    IonList,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonMenu,
+    IonMenuButton,
+    IonAvatar,
+    IonLabel,
+    IonItem,
+    TaskCardComponent,
+    IonButton,
+  ],
 })
 export class HomePage implements OnInit {
-todayTasks = [
-  {
-    title: "Math Assignment",
-    deadline: "Today 5 PM",
-    priority: "High"
-  },
-  {
-    title: "Physics Notes",
-    deadline: "Today 7 PM",
-    priority: "Medium"
-  }
-];
+  todayTasks = [
+    {
+      title: 'Math Assignment',
+      deadline: 'Today 5 PM',
+      priority: 'High',
+    },
+    {
+      title: 'Physics Notes',
+      deadline: 'Today 7 PM',
+      priority: 'Medium',
+    },
+  ];
 
-upcomingTasks = [
-  {
-    title: "Chemistry Lab",
-    deadline: "Tomorrow"
-  },
-  {
-    title: "English Essay",
-    deadline: "Friday"
+  upcomingTasks = [
+    {
+      title: 'Chemistry Lab',
+      deadline: 'Tomorrow',
+    },
+    {
+      title: 'English Essay',
+      deadline: 'Friday',
+    },
+  ];
+  
+  darkMode = false;
+
+toggleTheme() {
+  this.darkMode = !this.darkMode;
+
+  if (this.darkMode) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
   }
-];
+}
+
   constructor() {
     addIcons({
-      'grid-outline':gridOutline,
-      'book-outline':bookOutline,
-      'calendar-number-outline':calendarNumberOutline,
+      'grid-outline': gridOutline,
+      'book-outline': bookOutline,
+      'calendar-number-outline': calendarNumberOutline,
       'alarm-outline': alarmOutline,
       'stats-chart-outline': statsChartOutline,
       'timer-outline': timerOutline,
       'settings-outline': settingsOutline,
+      'sunny-outline': sunnyOutline,
+      'moon-outline': moonOutline,
+      'add-outline':addOutline,
+      'hourglass-outline':hourglassOutline,
       // 'checkmark-done-outline': checkmarkDoneCircleOutline
-    })
-   }
-
-  ngOnInit() {
+    });
   }
 
+  ngOnInit() {}
 }
