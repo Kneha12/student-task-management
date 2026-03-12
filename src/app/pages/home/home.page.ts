3156f2fe0da6f84c6c85f256ab0dc26bbbf060a7
Neha Kumari <117574008+Kneha12@users.noreply.github.com>
2026-03-12 11:14:13 +0530

    side bar completed

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,18 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenu, IonButton, IonMenuButton, IonButtons, IonAvatar, IonLabel, IonList, IonItem } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonMenu, IonButton, IonMenuButton, IonButtons, IonAvatar, IonLabel, IonList, IonItem, IonIcon, IonListHeader } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  gridOutline, 
+  bookOutline, 
+  calendarNumberOutline, 
+  alarmOutline, 
+  statsChartOutline, 
+  timerOutline, 
+  settingsOutline,
+  // checkmarkDoneCircleOutline 
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenu, IonButtons, IonMenuButton, IonAvatar, IonLabel, IonItem]
+  imports: [IonListHeader, IonIcon, IonList, IonButtons, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenu, IonButtons, IonMenuButton, IonAvatar, IonLabel, IonItem]
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      'grid-outline':gridOutline,
+      'book-outline':bookOutline,
+      'calendar-number-outline':calendarNumberOutline,
+      'alarm-outline': alarmOutline,
+      'stats-chart-outline': statsChartOutline,
+      'timer-outline': timerOutline,
+      'settings-outline': settingsOutline,
+      // 'checkmark-done-outline': checkmarkDoneCircleOutline
+    })
+   }
 
   ngOnInit() {
   }
